f231d54107a4b828ead29e84b020b25199406b48
prava <prava@LAPTOP-TV30L0L1>
2020-02-15 23:08:26 +0530

    sdf

--- samplehello/src/main/java/com/example/demo/SamplehelloApplication.java
+++ samplehello/src/main/java/com/example/demo/SamplehelloApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.controller")
 public class SamplehelloApplication {
 
 	public static void main(String[] args) {
